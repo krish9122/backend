d75942db5202277f84523cc9dc7f0ffbcabb7883
krish <coolkpatna.com>
2026-05-24 10:48:43 +0530

    ApiErrors , ApiResponse and asyncHandlers

---
+++ src/db/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError {
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        errors = [],
+        stack = ""
+    ) {
+        super(message)
+        this.errors = errors
+        this.statusCode = statusCode
+        this.data = null
+        this.success = false
+        this.message = message
+
+        if (stack) {
+            this.stack = stack
+        }else
+        {
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
---
+++ src/db/asyncHandlers.js
@@ -0,0 +1,7 @@
+const asyncHandlers = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export default asyncHandlers;
