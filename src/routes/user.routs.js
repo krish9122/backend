bf3de28b865e341efa2c9835fff09eececf30a5c
krish <coolkpatna.com>
2026-07-02 19:02:38 +0530

    routs for user model

--- src/routes/user.routs.js
+++ src/routes/user.routs.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, loggedOut, refreshTokens } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    loginUser,
+    loggedOut,
+    refreshTokens,
+    changeUserPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchedHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -9,7 +21,7 @@ userRouter.route("/register").post(
     upload.fields([
         {
             name: "avatar",
-            maxCount: 1, 
+            maxCount: 1,
         },
         {
             name: "coverImage",
@@ -24,5 +36,19 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT, loggedOut)
 
 userRouter.route("/refresh-token").post(refreshTokens)
+
+userRouter.route("/change-password").post(verifyJWT, changeUserPassword)
+
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser)
+
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+userRouter.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+userRouter.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+userRouter.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+
+userRouter.route("/history").get(verifyJWT, getWatchedHistory)
 
 export default userRouter;
